11b3def4a5654b2c0c4112b24d5b4dfc28b2a5d7
Ashirwad Pradhan <ashirwad.pradhan007@gmail.com>
2018-04-23 18:08:51 +0530

    added search functionality

--- new_app.py
+++ new_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import feedparser
 
 # init the app
@@ -9,6 +9,8 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
              'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
+#********************************************************
+# VIEWS USING STATIC URL'S
 # define the routes
 # @app.route('/bbc')
 # def bbc():
@@ -25,10 +27,14 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 # @app.route('/iol')
 # def iol():
 # 	return get_news(IOL_FEED_URL)
-@app.route('/')
-@app.route('/<channel>') # using dynamic routing here
-def get_news(channel='bbc'):
-	feed = feedparser.parse(RSS_FEEDS[channel])
+#**************************************************************
+
+#**************************************************************
+# VIEWS USING DYNAMIC URL'S
+# @app.route('/')
+# @app.route('/<channel>') # using dynamic routing here
+# def get_news(channel='bbc'):
+# 	feed = feedparser.parse(RSS_FEEDS[channel])
 	
 	# top_article = feed['entries'][0]
 
@@ -53,6 +59,19 @@ def get_news(channel='bbc'):
 
 	# now handle article via top_article object
 	# now we are passing all the news in the rss feeds
+	# return render_template('home.html', articles=feed['entries'])
+#**********************************************************************
+
+# using GET to request data rather than dynamic urls
+@app.route('/')
+def get_news():
+	query = request.args.get('channel')
+	if not query or query.lower() not in RSS_FEEDS:
+		channel = 'bbc'
+	else:
+		channel = query.lower()
+
+	feed = feedparser.parse(RSS_FEEDS[channel])
 	return render_template('home.html', articles=feed['entries'])
 
 
