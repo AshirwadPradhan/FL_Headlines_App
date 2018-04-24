ad6e8ba83481a204ccde56287494f5684b3ebc8f
Ashirwad Pradhan <ashirwad.pradhan007@gmail.com>
2018-04-24 13:25:08 +0530

    added weather and currency data

--- new_app.py
+++ new_app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request
 import feedparser
+import json
+import urllib.request
+import urllib.parse
+import api_key as ak
 
 # init the app
 app = Flask(__name__)
@@ -8,6 +12,11 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'cnn': 'http://rss.cnn.com/rss/edition.rss',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
              'iol': 'http://www.iol.co.za/cmlink/1.640'}
+
+DEFAULTS = {'channel':'bbc',
+            'city': 'London,UK',
+            'cur_frm':'GBP',
+            'cur_to':'USD'}
 
 #********************************************************
 # VIEWS USING STATIC URL'S
@@ -64,16 +73,84 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 
 # using GET to request data rather than dynamic urls
 @app.route('/')
-def get_news():
-	query = request.args.get('channel')
-	if not query or query.lower() not in RSS_FEEDS:
-		channel = 'bbc'
+def home():
+	'''
+	Route for home page
+	'''
+	# get the news data
+	channel = request.args.get('channel')
+	if not channel:
+		channel = DEFAULTS['channel']
+	articles = get_news(channel)
+
+	# get the city data
+	city = request.args.get('city')
+	if not city:
+		city = DEFAULTS['city']
+	weather = get_weather(city)
+
+	# get the currency data
+	cur_frm = request.args.get('cur_frm')
+	if not cur_frm:
+		cur_frm = DEFAULTS['cur_frm']
+
+	cur_to = request.args.get('cur_to')
+	if not cur_to:
+		cur_to = DEFAULTS['cur_to']
+
+	rate, currencies = get_conv_rate(cur_frm, cur_to)
+
+
+	return render_template('home.html', articles=articles, weather=weather, cur_frm=cur_frm, cur_to=cur_to, rate=rate, currencies=currencies)
+
+
+def get_news(channel):
+	'''
+	Get the news data based on channel
+	'''
+	if not channel or channel.lower() not in RSS_FEEDS:
+		channel = DEFAULTS['channel']
 	else:
-		channel = query.lower()
+		channel = channel.lower()
 
 	feed = feedparser.parse(RSS_FEEDS[channel])
-	return render_template('home.html', articles=feed['entries'])
 
+	return feed['entries']
+
+def get_weather(query):
+	'''
+	Get the current weather data from using openweather API
+	'''
+	api_url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid='+ak.OPENWEATHER_KEY
+	query = urllib.parse.quote(query)
+	url = api_url.format(query)
+	data_ = urllib.request.urlopen(url)
+	data = data_.read() # getting the call in bytes
+	encoding = data_.info().get_content_charset('utf-8') # getting the charset
+	parsed = json.loads(data.decode(encoding)) # decode the charset to str
+	weather = None
+	if parsed.get("weather"):
+		weather = {'description': parsed['weather'][0]['description'],
+		'temperature':parsed['main']['temp'],
+		'city':parsed['name'],
+		'country': parsed['sys']['country']}
+	return weather
+
+def get_conv_rate(frm, to):
+	'''
+	Get the current currency data from using openweather API
+
+	returns the conversion rate and all the currrency keys in the API
+	'''
+	api_url = 'https://openexchangerates.org//api/latest.json?app_id='+ak.OPENCURRENCY_KEY
+	data_ = urllib.request.urlopen(api_url)
+	data = data_.read() # getting the call in bytes
+	encoding = data_.info().get_content_charset('utf-8') # getting the charset
+	parsed = json.loads(data.decode(encoding)).get('rates') # decode the charset to str
+	frm_rate = parsed.get(frm.upper())
+	to_rate = parsed.get(to.upper())
+
+	return (to_rate/frm_rate, parsed.keys())
 
 # run the app
 if __name__ == '__main__':
